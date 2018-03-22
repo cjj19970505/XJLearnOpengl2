bae6412e3e9b440a9fd2aeb016f6c17009c71fac
陈俊嘉 <cjj19970505@live.cn>
2018-03-22 13:19:26 +0800

    s

--- XJLearnOpengl2/Mesh.h
+++ XJLearnOpengl2/Mesh.h
@@ -66,6 +66,7 @@ namespace xjg
 			glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
 			// set the vertex attribute pointers
 			// vertex Positions
+			
 			glEnableVertexAttribArray(0);
 			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
 			// vertex normals
